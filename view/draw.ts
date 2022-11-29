781cf3facc6f3b54a5f7ad15b60f41958d53b3f1
andjela-i <andjelailic28@gmail.com>
2022-11-29 02:07:01 +0100

    komit 4

--- view/draw.ts
+++ view/draw.ts
@@ -1,5 +1,10 @@
+import { combineLatest, forkJoin, fromEvent, map, Observable, switchMap } from "rxjs";
 import { knjige } from "../src";
 import { Book } from "../src/book";
+
+var ocene:number[]=[0,0,0,0,0];
+var obser:Observable<string>[]=[];
+var knjigeNas:Book[]=[]
 
     export var kontPretraga = document.createElement("div");
     document.body.appendChild(kontPretraga);
@@ -68,10 +73,40 @@ export function clearInput(unos:HTMLInputElement){
 }
 
 function getRandomBooks(){
-    var randKnjiga
+    var randKnjiga;
     for(let i=0;i<5;i++){
     randKnjiga = knjige[Math.floor(Math.random()*knjige.length)];
     console.log(randKnjiga)
-    drawBook(randKnjiga,polOceni)
+    obser[i]=drawBookZaOcenu(randKnjiga,polOceni)
     }
+    combineLatest([obser[0],obser[1],obser[2],obser[3],obser[4]]).subscribe((data)=>{
+        console.log(data);
+    })
+}
+
+export function drawBookZaOcenu(knjiga:Book,kontejner:HTMLDivElement):Observable<string>
+{
+
+    var div=document.createElement("div");
+    div.className="okvirKnjige";
+    kontejner.appendChild(div);
+    var lblNaslov=document.createElement("h3");
+    lblNaslov.textContent=knjiga.title;
+    var lblAutor=document.createElement("p");
+    lblAutor.textContent=knjiga.autor;
+    var input=document.createElement("input");
+    div.appendChild(lblNaslov);
+    div.appendChild(lblAutor);
+    var myImg=new Image();
+    myImg.src=knjiga.slika;
+    myImg.width=150;
+    myImg.height=200;
+    div.appendChild(myImg);
+    div.appendChild(input);
+    
+    var obs =fromEvent(input,"input").pipe(
+        map((ev:InputEvent)=>(<HTMLInputElement>ev.target).value)
+    );
+    return obs;
+
 }
